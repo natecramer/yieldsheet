9d916e7001af709c56f6e65822a317bad3f2006f
Nate Cramer <85828670+natecramer@users.noreply.github.com>
2025-04-22 21:48:18 -0400

    Add temperature calc prototype

---
+++ temp_script.js
@@ -0,0 +1,17 @@
+var room_temp = document.querySelector("#room-temp");
+console.log(room_temp);
+var mix_temp = document.querySelector("#mix-temp");
+var water_temp = document.querySelector("#water-temp");
+var btn_temp_calc = document.querySelector("#btn-temp-calc");
+
+room_temp.addEventListener("change", calcTemp, false);
+mix_temp.addEventListener("change", calcTemp, false);
+// room_temp.addEventListener("change", onTempInputChange, false);
+btn_temp_calc.addEventListener("click", calcTemp, false);
+
+function calcTemp(e) {
+    var room = parseInt(mix_temp.value);
+    var mix = parseInt(mix_temp.value);
+    var water = (80 * 3) - (room + mix + 25);
+    water_temp.value = parseInt(water);
+}
